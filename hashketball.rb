70e00aa5b5ec26c19cd1e9c8a32253c719e2a213
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 18:27:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,6 +3,31 @@ require 'pry'
 
 def game_hash
   
-  {}
+  {
+    home: {
+      team_name: "Brooklyn Nets", 
+      colors: ["Black", "White"], 
+      
+      players:{
+        
+      }
+      
+    }, 
+    
+    away: {
+      team_name: "Charlotte Hornets", 
+      colors: ["Turquoise", "Purple"], 
+      players:{
+        
+      }
+      
+    }
+      
+    
+    
+    
+    
+    
+  }
   
 end
