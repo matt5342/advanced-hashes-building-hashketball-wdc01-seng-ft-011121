41268a31e20e810341d7535ca72ae31fb163b4eb
Matthew Sewell <mattfsewell@gmail.com>
2020-12-07 19:04:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,10 @@ def game_hash
       players: [
         
         {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
-        {}
+        {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+        {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+        {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
+        {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
         
         
         ]
@@ -25,15 +28,7 @@ def game_hash
       colors: ["Turquoise", "Purple"], 
       players:[
         
-        :player_name => { }, 
-        :number => { }, 
-        :shoe => { }, 
-        :points => { }, 
-        :rebounds => { }, 
-        :assists => { }, 
-        :steals => { }, 
-        :blocks => { }, 
-        :slam_dunks => { }, 
+       {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22,:rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
             
         ]
         
